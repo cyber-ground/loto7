5cd536dadf84b962747c3ac4da4f98e9ee8fceea
cyber-ground <81938276+cyber-ground@users.noreply.github.com>
2025-01-20 17:47:32 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -44,6 +44,24 @@ const iconDownload = btnDownload.querySelector('.iconDownload');
 	let [newNumbers, insertedNumbers, currentJackpotNumbers] = [[],[],[]];
 	let [lastSlideIndex, storageValues, storageLength] = [[],[],[]];
 	
+	var insertHowl, swapHowl, resetHowl,undoHowl, slideHowl, faultHowl;
+	var uploadHowl, downloadHowl;
+	var callDeleteHowl, callDeleteAllHowl, deleteHowl, deleteAllHowl 
+
+	document.addEventListener('DOMContentLoaded', () => {
+		insertHowl = new Howl({src: ['mp3/insert.mp3'], volume: 0.2}); 
+		swapHowl = new Howl({src: ['mp3/swap.mp3'], volume: 0.1}); 
+		resetHowl = new Howl({src: ['mp3/reset.mp3'], volume: 0.2}); 
+		slideHowl = new Howl({src: ['mp3/slide.mp3'], volume: 0.1}); 
+		uploadHowl = new Howl({src: ['mp3/upload.mp3'], volume: 0.05}); 
+		downloadHowl = new Howl({src: ['mp3/download.mp3'], volume: 0.02}); 
+		callDeleteHowl = new Howl({src: ['mp3/callDelete.mp3'], volume: 0.3}); 
+		callDeleteAllHowl = new Howl({src: ['mp3/callDeleteAll.mp3'], volume: 0.03}); 
+		deleteHowl = new Howl({src: ['mp3/delete.mp3'], volume: 0.2}); 
+		deleteAllHowl = new Howl({src: ['mp3/deleteAll.mp3'], volume: 0.1}); 
+		undoHowl = new Howl({src: ['mp3/undo.mp3'], volume: 0.2}); 
+		faultHowl = new Howl({src: ['mp3/fault.mp3'], volume: 0.2}); 
+	});
 
 function init() {
 	if(portrait) { setCircleNumber()}
@@ -797,18 +815,18 @@ function assignCirclesColor() {
 	});
 }
 
-	var insertHowl = new Howl({src: ['mp3/insert.mp3'], volume: 0.2}); 
-	var swapHowl = new Howl({src: ['mp3/swap.mp3'], volume: 0.1}); 
-	var resetHowl = new Howl({src: ['mp3/reset.mp3'], volume: 0.2}); 
-	var slideHowl = new Howl({src: ['mp3/slide.mp3'], volume: 0.1}); 
-	var uploadHowl = new Howl({src: ['mp3/upload.mp3'], volume: 0.05}); 
-	var downloadHowl = new Howl({src: ['mp3/download.mp3'], volume: 0.02}); 
-	var callDeleteHowl = new Howl({src: ['mp3/callDelete.mp3'], volume: 0.3}); 
-	var callDeleteAllHowl = new Howl({src: ['mp3/callDeleteAll.mp3'], volume: 0.03}); 
-	var deleteHowl = new Howl({src: ['mp3/delete.mp3'], volume: 0.2}); 
-	var deleteAllHowl = new Howl({src: ['mp3/deleteAll.mp3'], volume: 0.1}); 
-	var undoHowl = new Howl({src: ['mp3/undo.mp3'], volume: 0.2}); 
-	var faultHowl = new Howl({src: ['mp3/fault.mp3'], volume: 0.2});
+	// var insertHowl = new Howl({src: ['mp3/insert.mp3'], volume: 0.2}); 
+	// var swapHowl = new Howl({src: ['mp3/swap.mp3'], volume: 0.1}); 
+	// var resetHowl = new Howl({src: ['mp3/reset.mp3'], volume: 0.2}); 
+	// var slideHowl = new Howl({src: ['mp3/slide.mp3'], volume: 0.1}); 
+	// var uploadHowl = new Howl({src: ['mp3/upload.mp3'], volume: 0.05}); 
+	// var downloadHowl = new Howl({src: ['mp3/download.mp3'], volume: 0.02}); 
+	// var callDeleteHowl = new Howl({src: ['mp3/callDelete.mp3'], volume: 0.3}); 
+	// var callDeleteAllHowl = new Howl({src: ['mp3/callDeleteAll.mp3'], volume: 0.03}); 
+	// var deleteHowl = new Howl({src: ['mp3/delete.mp3'], volume: 0.2}); 
+	// var deleteAllHowl = new Howl({src: ['mp3/deleteAll.mp3'], volume: 0.1}); 
+	// var undoHowl = new Howl({src: ['mp3/undo.mp3'], volume: 0.2}); 
+	// var faultHowl = new Howl({src: ['mp3/fault.mp3'], volume: 0.2});
 
 // -------------------------------------------------------------------------------
 //* ---------------------------------------
@@ -838,24 +856,7 @@ function assignCirclesColor() {
 
 // -------------------------------------------------------------------------------
 
-	// var insertHowl, swapHowl, resetHowl,undoHowl, slideHowl, faultHowl;
-	// var uploadHowl, downloadHowl;
-	// var callDeleteHowl, callDeleteAllHowl, deleteHowl, deleteAllHowl 
 
-	// document.addEventListener('DOMContentLoaded', () => {
-	// 	insertHowl = new Howl({src: ['mp3/insert.mp3'], volume: 0.2}); 
-	// 	swapHowl = new Howl({src: ['mp3/swap.mp3'], volume: 0.1}); 
-	// 	resetHowl = new Howl({src: ['mp3/reset.mp3'], volume: 0.2}); 
-	// 	slideHowl = new Howl({src: ['mp3/slide.mp3'], volume: 0.1}); 
-	// 	uploadHowl = new Howl({src: ['mp3/upload.mp3'], volume: 0.05}); 
-	// 	downloadHowl = new Howl({src: ['mp3/download.mp3'], volume: 0.02}); 
-	// 	callDeleteHowl = new Howl({src: ['mp3/callDelete.mp3'], volume: 0.3}); 
-	// 	callDeleteAllHowl = new Howl({src: ['mp3/callDeleteAll.mp3'], volume: 0.03}); 
-	// 	deleteHowl = new Howl({src: ['mp3/delete.mp3'], volume: 0.2}); 
-	// 	deleteAllHowl = new Howl({src: ['mp3/deleteAll.mp3'], volume: 0.1}); 
-	// 	undoHowl = new Howl({src: ['mp3/undo.mp3'], volume: 0.2}); 
-	// 	faultHowl = new Howl({src: ['mp3/fault.mp3'], volume: 0.2}); 
-	// });
 
 
 
